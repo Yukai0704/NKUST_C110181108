1dcf2860b010cc3794b22a0393e84ad95a5c9337
Yukai0704 <93572180+Yukai0704@users.noreply.github.com>
2021-11-09 16:39:45 +0800

    Add files via upload

---
+++ BMI.java
@@ -0,0 +1,7 @@
+public class BMI {
+    public static void main(String[] args) {
+        int weight = 77;
+        float height = 1.83f;
+        System.out.println(weight/(height*height));
+    }
+}
