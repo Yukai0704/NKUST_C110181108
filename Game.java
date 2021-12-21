10784bfe5043b9e8b9c0aff6ebe9c8bda19106fe
Yukai0704 <93572180+Yukai0704@users.noreply.github.com>
2021-12-21 16:20:27 +0800

    Add files via upload

--- Game.java
+++ Game.java
@@ -1,8 +1,53 @@
 public class Game {
-    int hp;
-    int mp;
-    int att;
+    Game(String job,double hp,double att,double MP){
+        this.job=job;
+        this.hp=hp;
+        this.att=att;
+        this.MP=MP;}
 
+    String job;//職位
+    double hp,att,MP;//生命，攻擊上限，護甲
 
+    public void attack(Game other){
+        double hurt = Math.random()*(att);
+        if(hurt<=other.MP)
+        else {
+            hurt = hurt - other.MP;;
+        }
+        other.hp-=hurt;
+
+        System.out.println(job+"對"+other.job+"造成了"+hurt+"點傷害");
+        System.out.println(" ");
+        System.out.println(job+"當前血量為:"+hp);
+        System.out.println(other.job+"當前血量為:"+other.hp);
+        System.out.println(" ");
+    }
+
+    public boolean live(Game other){
+        if(hp<=0) {
+            System.out.println(job+"已死亡    "+other.job+"獲得勝利");
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {Game p1=new Game("弓箭手",15,30,10);
+        Game p2=new Game("狂戰士",20,15,5);
+        Game p3=new Game("魔法師",10,40,30);
+        while(true)
+       {
+            p1.attack(p2);
+            if(!p2.live(p1)){
+                break;
+            }
+            p2.attack(p1);
+            if(!p1.live(p2)){
+                break;
+            }
+            p3.attack(p1);
+            if(!p1.live(p3)){
+                break;
+            }
+        }
     }
 }
