db3cc847b7c206d9bc7c2a1d9adc18250f50bd50
IgetyourgoatC110181113 <93572309+IgetyourgoatC110181113@users.noreply.github.com>
2021-12-21 14:30:24 +0800

    Update archer.java

--- archer.java
+++ archer.java
@@ -1,2 +1,9 @@
-public class archer {
+public class archer extends role{
+int HP;
+int MP;
+int ATT;
+String nmae;}
+
+  archer(String rolename,int roleHP, int roleMP, int roleATTACK) {super(rolename,roleHP, roleMP, roleATTACK);
+                                                                 }
 }
