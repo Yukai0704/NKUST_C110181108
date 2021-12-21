239cba4d487610d9a09cbb9da6134875b82dbeee
IgetyourgoatC110181113 <93572309+IgetyourgoatC110181113@users.noreply.github.com>
2021-12-21 14:52:49 +0800

    Update fighter.java

--- fighter.java
+++ fighter.java
@@ -1,2 +1,8 @@
-public class fighter {
+public class fighter extends role {
+int HP;
+int MP;
+int Att;
+String name;
+fighter(String rolename,int roleHP, int roleMP, int roleATTACK) {super(rolename,roleHP, roleMP, roleATTACK);
+                                                                }
 }
